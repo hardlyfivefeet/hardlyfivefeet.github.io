75313b866849cdf118d7193264bc53b81cd6cc98
Merissa Tan <Merissa@Merissas-MacBook-Pro.local>
2017-08-04 17:25:17 -0700

    Images

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 alert('Heya, welcome to my website!' );
 
-<script>
 var myIndex = 0;
 carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("homeSlides");
+    var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";
     }
@@ -15,4 +14,3 @@ function carousel() {
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
-</script>
